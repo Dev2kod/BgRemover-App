f6bbfd0486dc0b88cdc4e919015f139b2d20f1a7
Dev2kod <devesh4coding@gmail.com>
2025-04-14 11:39:50 +0530

    Creating repo

---
+++ react/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import Navbar from './components/Navbar'
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Navbar/>   
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/result' element={<Result/>}/>
+      <Route path='/buy' element={<BuyCredit/>}/>
+    </Routes>
+    </BrowserRouter> 
+  )
+}
+
+export default App
---
+++ react/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between py-3 mx-4 lg:mx-44 bg-gray-300'>
+        <Link to={'/'}><img src={assets.logo} alt="" className='w-32 sm:w-44' /></Link>
+        <button className='flex items-center gap-4 px-4 py-2 bg-zinc-800 rounded-full '>
+        <div className='text-amber-50'>Get Started  </div> <img className='ml-3 mt-1 w-3 h-5' src={assets.arrow_icon} alt="" />
+        </button>
+    </div>
+  )
+}
+
+export default Navbar
