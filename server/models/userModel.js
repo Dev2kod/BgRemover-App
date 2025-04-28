59898c6beda50f53f5b669f028218d9d43024370
Dev2kod <devesh4coding@gmail.com>
2025-04-28 22:22:25 +0530

    made default credits 5

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema = mongoose.Schema({
     photo:{type: String,unique:true},
     firstName:{type: String,unique:true},
     lastName:{type: String,unique:true},
-    creditBalance:{type:Number,unique:true}
+    creditBalance:{type:Number,default:5}
 })
 
 const userModel = mongoose.model("user",userSchema)
